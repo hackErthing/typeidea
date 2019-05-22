bd5b10acb532e4db7774d94c5e4635b3ce4c0e8b
whycee <whycee13@outlook.com>
2019-05-22 13:23:16 +0800

    config

---
+++ typeidea/comment/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+from blog.models import Post
+
+
+class Comment(models.Model):
+    STATUS_NORMAL = 1
+    STATUS_DELETE = 0
+    STATUS_ITEMS = (
+        (STATUS_NORMAL, '正常'),
+        (STATUS_DELETE, '删除')
+    )
+
+    target = 
